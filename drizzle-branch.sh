8df0dfb03f916c01dc032866775370fc7088faeb
Elan Ruusamäe <glen@pld-linux.org>
2009-03-14 19:18:54 +0000

    - cleaner version without local copies - need m4/bzr_version.m4 in tarball
    Changed files:
        drizzle-branch.sh -> 1.2

--- drizzle-branch.sh
+++ drizzle-branch.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-tar -cjf drizzle.tar.bz2 --exclude=.bzr drizzle
+set -e
+
+t=$(mktemp -d)
+p=$(basename $0 .sh)
+
+bzr clone pld-patches $t/$p
+
+# generate m4/bzr_version.m4
+cd $t/$p
+LIBTOOLIZE=true ACLOCAL=true AUTOHEADER=true AUTOMAKE=true AUTOCONF=true sh config/autorun.sh
+cd -
+
+tar -cjf $p.tar.bz2 -C $t --exclude=.bzr $p/
+
+rm -rf $t
